c9f4722477e2d833f8728ba8a35373456d41c633
millieeeeeeeeee <m20250715@gmail.com>
2025-09-05 11:31:16 +0800

    Update setting_text.py

--- utils/setting_text.py
+++ utils/setting_text.py
@@ -7,21 +7,29 @@ from utils.text_order import *
 from utils.user_message import *
 
 def choose_text_function(text):
+    # 把輸入清理掉特殊字元，只留數字/中英文
     search_member_text = re.sub(r"[^0-9\u4e00-\u9fffA-Za-z]", "", text)
     reply = None
-    # 會員ID/電話號碼查詢
-    if re.fullmatch(r"M\d{4}", text) or search_member_text.isdigit():
+
+    # 會員ID (M1234) 或電話號碼 (純數字)
+    if re.fullmatch(r"M\d{4}", search_member_text) or search_member_text.isdigit():
         reply = final_text_member(search_member_text)
-    # 商品編號查詢
-    elif re.fullmatch(r"P\d{4}", text):
-        reply = final_text_product(text)
-    # 訂單編號查詢   
-    elif re.fullmatch(r"O\d{4}", text):
-        reply = final_text_order(text)
-    
+
+    # 商品編號 (P1234)
+    elif re.fullmatch(r"P\d{4}", search_member_text):
+        reply = final_text_product(search_member_text)
+
+    # 訂單編號 (O1234)
+    elif re.fullmatch(r"O\d{4}", search_member_text):
+        reply = final_text_order(search_member_text)
+
+    # 會員姓名 (其他情況)
+    else:
+        reply = final_text_member(search_member_text)
+
     # 商品名稱模糊查詢
     if reply is None:
-        reply = final_text_product_diCt(text) 
+        reply = final_text_product_diCt(text)
 
     # 如果還是找不到
     if reply is None:
@@ -30,6 +38,4 @@ def choose_text_function(text):
             TextMessage(text=str(help()))     
         ]
     else:
-        return [reply]   
-
-
+        return [reply]
